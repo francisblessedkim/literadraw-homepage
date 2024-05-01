7bd77eff87fc731fdf133369327781b44da49e0e
Francis Blessed Kim <kimblessedfrancis@gmail.com>
2024-05-01 22:49:02 +0300

    update-gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,6 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        spaceId: "8qif2bdz3ayd",
-        accessToken: "h7pQiB8IoZ1a0PE3t6KY2fMCmZ2cmetAUFomrQBVg8s",
         name: "Gatsby Starter Contentful Homepage",
         short_name: "Gatsby",
         start_url: "/",
